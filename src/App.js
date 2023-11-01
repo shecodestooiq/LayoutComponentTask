904265e92faec9ec8ea3a060da56fe8530f74ce2
fatima2419 <126586807+fatima2419@users.noreply.github.com>
2023-10-31 23:09:32 -0300

    yaay

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import Layout from './Layout'
+
 function App() {
-  return <div>Layout Component Task</div>;
+  return <div>
+    
+    <Layout>
+    <h1>this is layout</h1>
+    </Layout>
+ 
+  </div>;
 }
 
 export default App;
