4d384aeca1da18c30ca148d268805121f6b11ccc
zhraa <zhraa2005spn@gmail.com>
2023-11-01 10:39:03 +0300

    layout component

---
+++ src/Component/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../index.css';
+const Main = () => {
+  return (
+    <main className="main">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed condimentum libero nec justo vulputate, non tristique arcu tincidunt. Vivamus eget efficitur nunc. Maecenas in libero eget lorem tincidunt tempor a id lectus. Sed auctor lacus non augue laoreet facilisis. Phasellus fermentum, mauris a tincidunt volutpat, arcu arcu facilisis nulla, ac auctor nunc velit a purus. Integer dictum augue nec justo gravida lacinia. Nullam interdum, justo eget vestibulum dictum, libero elit congue risus, vel sodales justo nulla sed lectus. Quisque sit amet nunc tristique, sollicitudin tellus a, egestas dui. Cras eget turpis eros. Integer vitae augue auctor, cursus tellus et, auctor ipsum. Nullam pharetra quam eget arcu vehicula, ut condimentum orci rhoncus. Sed quis dui id nisl malesuada auctor ut ut massa. Duis volutpat justo a justo cursus volutpat.
+    </main>
+  );
+}
+
+export default Main;
---
+++ src/Component/SideBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../index.css';
+
+const SideBar = () => {
+  return (
+    <div className="sidebar">
+      <ul>
+        <li>home</li>
+        <li>about</li>
+        <li>details</li>
+      </ul>
+    </div>
+  );
+}
+
+export default SideBar;
